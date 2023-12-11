1216293babd1d40a4ab32795ff71d2e9b62e72b6
amalrxj <amalrajkvdev@gmail.com>
2023-12-11 13:22:18 +0530

    nav_animation

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".home__title__type", {
+    strings: ["Full Stack Dev", "Python Developer", "Web Designer","NodeJS Dev"],
+    typeSpeed: 150,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+
+})

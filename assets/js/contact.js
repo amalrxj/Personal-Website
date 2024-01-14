8da3260960611f8b6b738ed692863b8496616e28
Amal Raj <amalrajkvdev@gmail.com>
2024-01-14 06:44:37 +0530

    documentation

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,25 +3,20 @@ const contactForm = document.getElementById('contact-form'),
 
 const sendEmail = (e) => {
     e.preventDefault()
-
     //ServiceID, TemplateID, #form, publickey
     emailjs.sendForm('service_c0n6gpa', 'template_axvxtb9', '#contact-form', 'ldlAFMA6vlTrKV6V7')
         .then(() => {
             //show sent message
             contactMessage.textContent = 'Message sent successfully ✅'
-
             //Remove message after five seconds
             setTimeout(() => {
                 contactMessage.textContent = ''
             }, 5000)
-
             //Clear input field
             contactForm.reset()
         }, () => {
-
             //Show error message
             contactForm.textContent = 'Message not sent (service error) ❌'
         })
 }
-
 contactForm.addEventListener('submit', sendEmail)
